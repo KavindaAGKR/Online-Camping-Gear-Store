9075dbd909ff3e2ee5a7a751a254d0c43997b5bb
KavindaAGKR <kavindaaludeniya@gmail.com>
2024-06-27 20:53:18 +0530

    errors fixed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const port = process.env.PORT;
-const app = require('./app');
+const app = require('./src/app');
 // const cors = require('cors');
 
 
