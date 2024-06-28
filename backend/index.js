6c05edb26518ffd2702f3fab525195d8133210d2
KavindaAGKR <kavindaaludeniya@gmail.com>
2024-06-28 07:58:04 +0530

    Docker image created

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,23 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const port = process.env.PORT;
 const app = require('./src/app');
 // const cors = require('cors');
 
+const PORT = process.env.PORT || 5000;
+
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://kavindaaludeniya:BLNihyjL1zpdBN8R@cluster0.nuwbka2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// Connect to MongoDB
+mongoose.connect(dbURI, {})
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
 
-
-
-mongoose.connect(
-    process.env.MONGO_URI,
-    {}).then(result => {
-        console.log("db conntected")
-        app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`)
-        })
-    }
-    ).catch(err => console.log(err))
-
-    
